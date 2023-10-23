99eee67e72ae506bce06d8ee242b8a7f80c939e7
Daniel Felipe Gomez Aristizabal <alu0101438139@ull.edu.es>
2023-10-23 19:06:48 +0100

    Corregidos los errores en el fichero server.ts, errores de sintaxis

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,26 +5,25 @@ import { connectToDatabase } from "./database";
 import { employeeRouter } from "./employee.routes";
 
 // Load environment variables from the .env file, where the ATLAS_URI is configured
-dotenv.();
+dotenv.config();
 
 const { ATLAS_URI } = process.env;
 
 if (!ATLAS_URI) {
     console.error("No ATLAS_URI environment variable has been defined in config.env");
-    process.e(1);
+    process.exit(1);
 }
 
-connectToDa(ATLAS_URI)
+connectToDatabase(ATLAS_URI)
     .then(() => {
         const app = express();
         app.use(cors());
         app.use("/employees", employeeRouter);
 
         // start the Express server
-        app.listen(52
+        app.listen(5200, () => {
             console.log(`Server running at http://localhost:5200...`);
         });
 
     })
     .catch(error => console.error(error));
-    error
