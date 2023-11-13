a4c2f65555b2e77375d6b1b5d72af425067f4135
facu2002 <alu0101406329@ull.edu.es>
2023-11-13 17:31:43 +0000

    Conexión de la base de datos con el código, creado el esqueleto del usuario en la base de datos

--- server/src/routers/user/post.ts
+++ server/src/routers/user/post.ts
@@ -8,6 +8,7 @@ import { User } from "../../models/userModel.js";
 export const postUser = async (req: any, res: any) => {
   try {
     const user = new User(req.body);
+    await user.save();
     return res.status(201).send(user);
   } catch (error) {
     return res.status(500).send({msg: "No se añadió correctamente el usuario", error: error});
