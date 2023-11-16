5680dcade6d215e58dc41183586b973a4d6b6379
facu2002 <alu0101406329@ull.edu.es>
2023-11-16 18:36:33 +0000

    Estructura del formulario de login realizada

--- client/TastyBite/src/components/LoginForm.tsx
+++ client/TastyBite/src/components/LoginForm.tsx
@@ -8,9 +8,7 @@ export function LoginForm() {
         <a href="#">Tasty Bite</a> &gt; <a href="#">Iniciar sesión</a>
       </div>
       <div className="login-form-container">
-        <div className="login-form-container__left-container">
-          <img src="../images/IniciarSesionImagen.webp" alt="Login" />
-        </div>
+
         <div className="login-form-container__right-container">
           <h1>Iniciar Sesión</h1>
           <form className="form-login">
@@ -26,8 +24,10 @@ export function LoginForm() {
             <div className="login-form-container__right-container__register">
               <p>¿No tienes cuenta? <a href="#">Registrate</a></p>
             </div>
-            <img className='logo-cocodrilo' src='../images/LogoCocodrilo.png'/>
           </form>
+        </div>
+        <div className="login-form-container__left-container">
+          <img src="../images/Cocodrilo.png" alt="Cocodrilo señalando login" />
         </div>
       </div>
     </>
