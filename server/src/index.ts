1fdf20579071b7df9ef2428f754b5b3d48c1310f
facu2002 <alu0101406329@ull.edu.es>
2023-11-28 09:42:05 +0000

    Despliegue de la aplicación

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,18 @@ import { app } from './app.js';
 
 const port = process.env.PORT || 3000;
 
+
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
+
+
+
+
+
+// const port = 8081;
+// const host = '10.6.128.69';
+
+// app.listen(port, host, () => {
+//   console.log(`Server started at http://${host}:${port}`);
+// });
